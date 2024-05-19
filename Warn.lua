a63e3aac03fb6f1418cb862842e298712d41d5e5
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-19 14:26:04 +0800

    Create Warn.lua

---
+++ Warn.lua
@@ -0,0 +1,7 @@
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	Instance.new = "Spawn"
+	cue2.SoundId = "rbxassetid://9125713501"
+	cue2.Volume = 999999998999
+	cue2.PlaybackSpeed = 0.5
+	cue2:Play()
